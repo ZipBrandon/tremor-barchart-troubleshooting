b2a0eecd6dfcf748db34e3e16cd32187a4a92ffe
Brandon Lorenz <blorenz@gmail.com>
2024-05-20 14:06:01 -0400

    testing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import { BarChartHero } from '~/@/BarChartHero';
+import { BarListHero, SparkChartHero } from '~/@/BarListHero';
+import { Example } from '~/@/TestComponents';
+import { AnotherComponent } from '~/@/AnotherComponent';
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,32 +14,22 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1 className={`text-blue-500 text-5xl`} >Welcome to Remix</h1>
+      <div>
+        <Example />
+      </div>
+      <div>
+        <AnotherComponent />
+      </div>
+      <div>
+        <BarChartHero />
+      </div>
+      <div>
+        <BarListHero />
+      </div>
+      <div>
+        <SparkChartHero />
+      </div>
     </div>
   );
 }
